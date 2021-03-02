b6010edd2fdf06fa1897f96bb218db5679015568
calebjenkins <caleb@calebjenkins.com>
2021-03-01 20:05:34 -0600

    Added some click checking, reset and messages

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squares = document.querySelectorAll('.grid div')
   const playerDisplay = document.querySelector('#player')
+  const messageLabel = document.querySelector('#message')
+  const resetButton = document.querySelector('#clearBtn');
 
   let currentPlayer = 'playerX'
+
+  resetButton.addEventListener('click', clearBoard);
 
   squares.forEach(square => {
     square.addEventListener('click', clickOutcome)
   })
 
   function clickOutcome(e) {
-    const squareArray = Array.from(squares)
-    const index = squareArray.indexOf(e.target)
-    playerDisplay.innerHTML = currentPlayer
+    const squareArray = Array.from(squares);
+    const index = squareArray.indexOf(e.target);
+    playerDisplay.innerHTML = currentPlayer;
+    messageLabel.innerHTML = "";
+
+    if(squares[index].classList.contains('playerX') || squares[index].classList.contains('playerO'))
+    {
+      console.log('already selected - bale out!');
+      playerDisplay.innerHTML = currentPlayer;
+      messageLabel.innerHTML = "square already selected";
+      return;
+    }
 
     if(currentPlayer === 'playerX') {
       squares[index].classList.add('playerX')
@@ -20,14 +33,25 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[index].classList.add('playerO')
       currentPlayer = 'playerX'
     }
+
+    playerDisplay.innerHTML = currentPlayer;
   }
 
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      return response.json()
-    })
-    .then(users => {
-      console.log(users)
-    })
+  function clearBoard(e)
+  {
+    squares.forEach(square => {
+      if(square.classList.contains('playerX'))
+      {
+        square.classList.remove('playerX')
+      }
+      if(square.classList.contains('playerO'))
+      {
+        square.classList.remove('playerO')
+      }
+    });
 
+    playerDisplay.innerHTML = currentPlayer;
+    messageLabel.innerHTML = "Game Reset";
+    return;
+  }
 })
